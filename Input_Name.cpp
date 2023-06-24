b6e3f65e8afcec749f910dd8ebe786900b35f846
laye6 <laye6camara@gmail.com>
2023-06-24 16:49:14 -0400

    init

---
+++ Input_Name.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string lastName, firstName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Hello, " << firstName << " " << lastName << "!" << endl;
+
+    return 0;
+}
